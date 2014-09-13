8dea8772ba5c949700355a6e5335d4c60d2073d6
RyanSuen <1454416761@qq.com>
2014-09-13 17:21:49 +0800

    node/blog
    blog/express

---
+++ blog/models/user.js
@@ -0,0 +1,68 @@
+var mongodb = require('./db');
+
+var User = function(user) {
+	this.name = user.name;
+	this.password = user.password;
+	this.email = user.email;
+};
+
+module.exports = User;
+
+User.prototype.save = function(callback) {
+
+	//要存入数据库的用户文档
+	var user = {
+		name: this.name,
+		password: this.password,
+		email: this.email
+	};
+
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+
+		db.collection('users', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			collection.insert(user, {
+				safe: true
+			}, function(err, user) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null,user[0]);
+			});
+		});
+
+
+	});
+
+};
+
+User.get = function(name, callback) {
+	mongodb.open(function(err, db) {
+		if(err) {
+			callback(err);
+		}
+		db.collection('users', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+			collection.findOne({
+				name: name
+			}, function(err, user) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null,user);
+			});
+		});
+	});
+};
